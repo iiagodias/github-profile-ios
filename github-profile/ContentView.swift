35159e8315f2041fba3ee14941bcc53363f9ca63
Iago Dias <iago.unama@hotmail.com>
2023-06-12 14:33:07 -0300

    Layout dos cards

--- github-profile/ContentView.swift
+++ github-profile/ContentView.swift
@@ -9,18 +9,56 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        ZStack{
+            Color("Primary")
+            
+            ScrollView{
+                VStack(spacing: 16){
+                    CardRepository()
+                    CardRepository()
+                }
+                .padding(16)
+               
+            }
+            
         }
-        .padding()
+        .ignoresSafeArea()
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CardRepository: View {
+    var body: some View {
+        VStack{
+            VStack(alignment: .leading, spacing: 8) {
+                Text("Titulo do repositorio")
+                    .foregroundColor(Color("Secondary"))
+                Text("Descrição do Repo")
+                    .foregroundColor(Color("Secondary"))
+                HStack{
+                    Image("user")
+                    Text("Iago Dias")
+                        .foregroundColor(Color("Secondary"))
+                }
+                
+            }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(16)
+            .background(Color("Porcelain"))
+            .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(Color("SilverSand"), lineWidth: 4)
+            )
+            .cornerRadius(8)
+            
+        }
+
     }
 }
