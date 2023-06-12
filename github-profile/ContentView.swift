b79b8628c8ed5aace40082201fd727dac6b0cb4e
Iago Dias <iago.unama@hotmail.com>
2023-06-12 14:53:14 -0300

    Ajustes de scrollView

--- github-profile/ContentView.swift
+++ github-profile/ContentView.swift
@@ -11,19 +11,21 @@ struct ContentView: View {
     var body: some View {
         
         ZStack{
-            Color("Primary")
-            
+            ZStack{
+                Color("Primary")
+            }.ignoresSafeArea()
+                       
             ScrollView{
                 VStack(spacing: 16){
-                    CardRepository()
-                    CardRepository()
+                    
+                    ForEach(0..<50) { item in
+                        CardRepository()
+                    }
                 }
                 .padding(16)
-               
             }
-            
+        
         }
-        .ignoresSafeArea()
         
     }
 }
@@ -36,29 +38,31 @@ struct ContentView_Previews: PreviewProvider {
 
 struct CardRepository: View {
     var body: some View {
-        VStack{
-            VStack(alignment: .leading, spacing: 8) {
-                Text("Titulo do repositorio")
-                    .foregroundColor(Color("Secondary"))
-                Text("Descrição do Repo")
-                    .foregroundColor(Color("Secondary"))
-                HStack{
-                    Image("user")
-                    Text("Iago Dias")
+        Button(action: {}) {
+            VStack{
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Titulo do repositorio")
                         .foregroundColor(Color("Secondary"))
+                    Text("Descrição do Repo")
+                        .foregroundColor(Color("Secondary"))
+                    HStack{
+                        Image("user")
+                        Text("Iago Dias")
+                            .foregroundColor(Color("Secondary"))
+                    }
+                    
                 }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(16)
+                .background(Color("Porcelain"))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 8)
+                        .stroke(Color("SilverSand"), lineWidth: 4)
+                )
+                .cornerRadius(8)
                 
-            }
-            .frame(maxWidth: .infinity, alignment: .leading)
-            .padding(16)
-            .background(Color("Porcelain"))
-            .overlay(
-                RoundedRectangle(cornerRadius: 8)
-                    .stroke(Color("SilverSand"), lineWidth: 4)
-            )
-            .cornerRadius(8)
-            
-        }
+           }
+      }
 
     }
 }
