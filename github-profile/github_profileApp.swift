3589239a53643692fc9b80fde023a8bf6405b789
Iago Dias <iago.unama@hotmail.com>
2023-06-12 13:48:51 -0300

    Initial Commit

---
+++ github-profile/github_profileApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_profileApp.swift
+//  github-profile
+//
+//  Created by Iago Dias on 12/06/23.
+//
+
+import SwiftUI
+
+@main
+struct github_profileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
